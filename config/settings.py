faa3fc856376678c3e27d5161949fae7e878c6c1
Bekzod <toshpulatovbekki7@icloud.com>
2023-05-24 16:18:41 +0500

    changed configuration

--- config/settings.py
+++ config/settings.py
@@ -204,4 +204,4 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
-AUTHENTICATION_SERVICE = 'https://auth-sevice.up.railway.app'
+AUTHENTICATION_SERVICE = 'https://pbl-auth-sevice.up.railway.app'
